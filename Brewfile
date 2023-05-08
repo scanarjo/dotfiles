1fde0bb5097424a17424eadf7855629ae6ff951e
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2023-05-08 17:15:59 +0100

    Switch to using brew bundle for installs

---
+++ Brewfile
@@ -0,0 +1,15 @@
+# Taps
+tap "homebrew/bundle"
+
+# Brews
+brew "bat"
+brew "exa"
+brew "httpie"
+
+# Casks
+cask "1password"
+cask "alfred"
+cask "google-chrome"
+cask "opera-gx"
+cask "readdle-spark"
+cask "visual-studio-code"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,16 +4,6 @@ echo "\nInstalling Homebrew...\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "\nInstalling formulae...\n"
+echo "\nInstalling packages...\n"
 
-brew install bat
-brew install httpie
-
-echo "\nInstalling casks...\n"
-
-brew install --cask 1password
-brew install --cask visual-studio-code
-brew install --cask google-chrome
-brew install --cask opera-gx
-brew install --cask alfred
-brew install --cask readdle-spark
+brew bundle
