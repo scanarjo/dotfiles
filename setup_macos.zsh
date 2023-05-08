bb509a6df5c882e20e43c1da1fe21d10376e7fb4
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2023-05-08 19:08:04 +0100

    Add script for configuring macos preferences

---
+++ setup_macos.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+echo "\nSetting up MacOS...\n"
+
+# Ensure System Preferences is closed
+osascript -e 'tell application "System Preferences" to quit'
+
+# Trackpad
+defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
+defaults write com.apple.AppleMultitouchTrackpad Dragging -int 1
+
+# Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Dock
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock tilesize -int 42
+defaults write com.apple.dock largesize -int 72
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock "orientation" -string "left"
+defaults write com.apple.dock "static-only" -bool "true"
+
+# Screencapture
+defaults write com.apple.screencapture location ~/Pictures/Screenshots
+
+# Kill affected applications
+killall Dock
+killall Finder
+killall SystemUIServer
+
+echo "\nMacOS Setup Complete. A logout or restart might be necessary.\n"
