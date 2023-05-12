4fddd3749e5e7f5f031e227ca7ca4d09eb6e4dd2
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2023-05-12 16:11:25 +0100

    Fix dragging setup

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -7,7 +7,8 @@ osascript -e 'tell application "System Preferences" to quit'
 
 # Trackpad
 defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
-defaults write com.apple.AppleMultitouchTrackpad Dragging -int 1
+defaults write com.apple.AppleMultitouchTrackpad Dragging -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -bool true
 
 # Finder
 defaults write com.apple.finder ShowPathbar -bool true
@@ -36,5 +37,7 @@ defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Blueto
 killall Dock
 killall Finder
 killall SystemUIServer
+
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
 echo "\nMacOS Setup Complete. A logout or restart might be necessary.\n"
